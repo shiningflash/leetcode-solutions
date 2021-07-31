6054fddafb93ddc13f38214d1b9eb1dc06ce900f
Amirul Islam <amirulislamalmamun@gmail.com>
2021-08-01 02:01:52 +0600

    valid parenthesis

---
+++ Algorithms/Easy/20. Valid Parentheses/Solution.py
@@ -0,0 +1,32 @@
+"""
+Approach: stack
+
+91 / 91 test cases passed.
+Status: Accepted
+
+Runtime: 32 ms
+Memory Usage: 14.4 MB
+
+Problem link: https://leetcode.com/problems/valid-parentheses
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c in "({[":
+                stack.append(c)
+                continue
+                
+            if not stack:
+                return False
+            
+            pop = stack.pop()
+            if pop == '(' and c == ')':
+                continue
+            elif pop == '{' and c == '}':
+                continue
+            elif pop == '[' and c == ']':
+                continue
+            return False
+        return not stack
