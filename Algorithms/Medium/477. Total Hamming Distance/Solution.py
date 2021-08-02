5422e64824395c8fdb8c617e9976815f950fae8b
Amirul Islam <amirulislamalmamun@gmail.com>
2021-08-03 03:01:20 +0600

    Total Hamming Distance

---
+++ Algorithms/Medium/477. Total Hamming Distance/Solution.py
@@ -0,0 +1,23 @@
+"""
+Approach: bitwise operation
+
+
+
+46 / 46 test cases passed.
+Status: Accepted
+
+Runtime: 492 ms
+Memory Usage: 15.3 MB
+
+Problem link: https://leetcode.com/problems/total-hamming-distance
+"""
+
+class Solution:
+    def totalHammingDistance(self, nums: List[int]) -> int:
+        tot = 0
+        for x in range(0, 32):
+            set_bit = 0
+            for val in nums:
+                set_bit += (val >> x) & 1
+            tot += set_bit * (len(nums) - set_bit)
+        return tot
