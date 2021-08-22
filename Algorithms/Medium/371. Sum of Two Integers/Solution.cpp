c1815667e7304731d93b2ce066ab4d31e412c89f
Amirul Islam <amirulislamalmamun@gmail.com>
2021-08-23 00:46:52 +0600

    sum of two int

---
+++ Algorithms/Medium/371. Sum of Two Integers/Solution.cpp
@@ -0,0 +1,25 @@
+/*
+Approach: AND, XOR
+
+
+
+13 / 13 test cases passed.
+Status: Accepted
+
+Runtime: 0 ms
+Memory Usage: 6 MB
+
+Problem link: https://leetcode.com/problems/sum-of-two-integers/
+*/
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b) {
+            unsigned c = a & b; // carry value
+            a = a ^ b; // xor value
+            b = c << 1; // shift carry value
+        }
+        return a;
+    }
+};
