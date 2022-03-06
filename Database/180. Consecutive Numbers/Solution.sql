477ace2655891cafad9fcd07cad75b42eaa6b7b6
Amirul Islam <amirulislamalmamun@gmail.com>
2022-03-07 01:30:46 +0600

    database added

---
+++ Database/180. Consecutive Numbers/Solution.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+SELECT
+    DISTINCT l1.num AS ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.id = l2.id - 1 and
+    l2.id = l3.id - 1 and
+    l1.num = l2.num and
+    l2.num = l3.num;
